db2e3f4e53dc5e3d9b1c6ca9526536e4bbc7add6
Aris Anninga <88113610+AEAnninga@users.noreply.github.com>
2021-09-02 16:39:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+let firstName = 'Aris';
+let sumNumbers = 4 + 4;
+let sumStrings = '4' + '4';
+console.log(sumNumbers);
+console.log(sumStrings);
+console.log(50 + 50);                       // 100
+console.log(50 - 50);                       // 0
+console.log(50 * 50, 50 / 50, 50 % 50);     // 2500, 1, 0
+let age = '39';
+typeof age;
+console.log(typeof age);
+console.log(age);
